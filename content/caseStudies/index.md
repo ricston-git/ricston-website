b3eadd615345ff0ee4fa5a0428d97931c8255312
Justin Calleja <justin.calleja@ricston.com>
2014-10-15 15:45:29 +0200

    Added Case studies filler page and moved over all .active logic to grey.js

---
+++ content/caseStudies/index.md
@@ -0,0 +1,6 @@
+---
+draft = false
+title = "Case Studies"
+date = 2014-05-20T10:04:31Z
+---
+# Case Studies
--- themes/ricstonTheme/static/js/tmp/grey.js
+++ themes/ricstonTheme/static/js/tmp/grey.js
@@ -7,11 +7,29 @@
         }
 
         if(pathname === "/" || pathname === "/aboutUs/" || pathname === "/ourServices/") {
+
             addActiveClass("#ricston-home-nav-menu");
+
         } else if(pathname.lastIndexOf("/industries/", 0) === 0) {
+
             addActiveClass("#ricston-industries-nav-menu");
+
         } else if(pathname.lastIndexOf("/tech/", 0) === 0) {
+
             addActiveClass("#ricston-tech-nav-menu");
+
+        } else if(pathname.lastIndexOf("/caseStudies/", 0) === 0) {
+
+            addActiveClass("#ricston-casestudies-nave-menu");
+
+        } else if(pathname.lastIndexOf("/integration/", 0) === 0) {
+
+            addActiveClass("#ricston-integration-nav-menu");
+
+        } else if(pathname.lastIndexOf("/contactUs/", 0) === 0) {
+
+            addActiveClass("#ricston-contactus-nav-menu");
+
         }
 
         

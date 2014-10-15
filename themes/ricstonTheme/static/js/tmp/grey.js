975f3a4ee32940b9bf86254ffad443e114ab4975
Justin Calleja <justin.calleja@ricston.com>
2014-10-15 15:34:05 +0200

    Added /aboutUs/ and /ourServices/ awareness to grey.js

--- themes/ricstonTheme/static/js/tmp/grey.js
+++ themes/ricstonTheme/static/js/tmp/grey.js
@@ -1,17 +1,14 @@
 (function($, url) {
     $(function() {
         var pathname = url.pathname;
-        // console.log("pathname is: " + pathname);
 
         var addActiveClass = function(idStr) {
             $(idStr).addClass("active");
         }
 
-        if(pathname === "/") {
+        if(pathname === "/" || pathname === "/aboutUs/" || pathname === "/ourServices/") {
             addActiveClass("#ricston-home-nav-menu");
-
         } else if(pathname.lastIndexOf("/industries/", 0) === 0) {
-            // $("#ricston-industries-list").addClass("active");
             addActiveClass("#ricston-industries-nav-menu");
         } else if(pathname.lastIndexOf("/tech/", 0) === 0) {
             addActiveClass("#ricston-tech-nav-menu");
